9b9417fecaf148e92774ff3119090506399fd83b
unknown <mahmudul.cse.eng@gmail.com>
2024-11-10 19:47:36 +0600

    popular-product, book-table and footer is implemented

--- main.js
+++ main.js
@@ -24,16 +24,19 @@ $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:10,
-        nav:true,
+        
+        autoplay:true,
         responsive:{
             0:{
                 items:1
             },
             600:{
-                items:3
+                items:2,
+                nav:true,
             },
             1000:{
-                items:4
+                items:4,
+                nav:true,
             }
         }
     })
