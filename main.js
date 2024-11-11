e55859e43b7c638a7e2f02b6d76d45d3a8003d57
unknown <mahmudul.cse.eng@gmail.com>
2024-11-12 00:45:54 +0600

    customers-say section implemented and resonsive for the different devices

--- main.js
+++ main.js
@@ -21,14 +21,15 @@ window.addEventListener('scroll', () => {
 
 
 $(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
+    $('.popular-product .owl-carousel').owlCarousel({
         loop:true,
         margin:10,
         
         autoplay:true,
         responsive:{
             0:{
-                items:1
+                items:1,
+                nav:true,
             },
             600:{
                 items:2,
@@ -36,6 +37,28 @@ $(document).ready(function () {
             },
             1000:{
                 items:4,
+                nav:true,
+            }
+        }
+    })
+
+// customers say carousel
+    $('.customers-say .owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        
+        autoplay:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true,
+            },
+            600:{
+                items:1,
+                nav:true,
+            },
+            1000:{
+                items:1,
                 nav:true,
             }
         }
